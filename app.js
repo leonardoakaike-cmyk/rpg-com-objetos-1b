57ee04c9d3aaa25e25eb7c7052df77b0e56e4970
Marques081210 <orbanaruro@gmail.com>
2026-08-12 11:24:36 -0300

    first commit

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const guerreiro = {
-nome: "Thorin",
-vida: 60,
-ataque: 20,
-causaDano: function(personagem){
-personagem.recebeDano(this.ataque)
+class Personagem {
+    constructor(nome, vida, ataque) {
+        this.nome = nome,
+        this.vida = vida,
+        this.ataque = ataque
+    }
+
+    causaDano(personagem) {
+        personagem.recebeDano(this.ataque)
+    }
+
+    recebeDano(quantidade){
+        this.vida = this.vida - quantidade
+
+        if(this.vida <= 0){
+            this.vida = "Morreu"
+        }
+    }
 }
-}
-const mago = {
-nome: "Gandalfe",
-vida: 60,
-ataque: 35,
-recebeDano: function(quantidade){
-this.vida = this.vida - quantidade
-}
-}
-const arqueiro = {
-nome: "Legolas",
-vida: 80,
-ataque: 25
-}
-guerreiro.causaDano(mago)
-console.log(mago.vida)  
+
+const druida = new Personagem ("Obin", 150, 15)
+const guerreiro = new Personagem ("Thorin", 60, 20)
+const arqueiro = new Personagem ("Legolas", 80, 25)
+const mago = new Personagem ("Gandalfe", 60, 35)
+const japones = new Personagem ("Leonardo", 2, 0.5)
